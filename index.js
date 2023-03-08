2d45b720a0fd12b83f4179356adab4fc6df1f698
Sharath S <17112360+ki11e6@users.noreply.github.com>
2023-03-08 14:08:32 +0530

    secondt commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.traversymedia.com/modern-javascript-2-0');
+  await page.goto('https://www.traversymedia.com/');
 
   //   for screenshot
   //   await page.screenshot({ path: 'ex.png', fullPage: true });
@@ -18,8 +19,29 @@ async function run() {
   //   const title = await page.evaluate(() => document.title);
 
   // for text from page
-  const text = await page.evaluate(() => document.body.innerText);
-  console.log(text);
+  //   const text = await page.evaluate(() => document.body.innerText);
+
+  // for links from the page
+  //   const links = await page.evaluate(() =>
+  //     Array.from(document.querySelectorAll('a'), (e) => e.href)
+  //   );
+
+  //for courses list from page
+  const courses = await page.evaluate(() =>
+    Array.from(document.querySelectorAll('#cscourses .card'), (e) => ({
+      title: e.querySelector('.card-body h3').innerText,
+      level: e.querySelector('.card-body .level').innerText,
+      url: e.querySelector('.card-footer a').href,
+    }))
+  );
+
+  console.log(courses);
+
+  //save data to JSON file
+  fs.writeFile('courses.json', JSON.stringify(courses), (err) => {
+    if (err) throw err;
+    console.log('File saved');
+  });
 
   await browser.close();
 }
